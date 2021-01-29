a1c37ca77d4788572e6a9cf9665643744c2032c0
kakin <ielts_go3@163.com>
2021-01-29 10:19:13 +0800

    leetcode 7

---
+++ src/test/java/com/github/uguisu/www/algorithm/leetCode/question007/TestCase.java
@@ -0,0 +1,43 @@
+package com.github.uguisu.www.algorithm.leetCode.question007;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCase {
+
+    @Test
+    public void test001() {
+        Solution s = new Solution();
+        int input = 123;
+        assertEquals(321, s.reverse(input));
+    }
+
+    @Test
+    public void test002() {
+        Solution s = new Solution();
+        int input = -123;
+        assertEquals(-321, s.reverse(input));
+    }
+
+    @Test
+    public void test003() {
+        Solution s = new Solution();
+        int input = 120;
+        assertEquals(21, s.reverse(input));
+    }
+
+    @Test
+    public void test004() {
+        Solution s = new Solution();
+        int input = 0;
+        assertEquals(0, s.reverse(input));
+    }
+    @Test
+    public void test005() {
+        Solution s = new Solution();
+        int input = 1534236469;
+        assertEquals(0, s.reverse(input));
+    }
+
+}
