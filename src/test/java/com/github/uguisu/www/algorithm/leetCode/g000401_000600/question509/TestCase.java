54c9c070f154caa9925d030b422d1d4ef28c152a
kakin <ielts_go3@163.com>
2021-02-08 00:17:39 +0800

    leetcode 509

---
+++ src/test/java/com/github/uguisu/www/algorithm/leetCode/g000401_000600/question509/TestCase.java
@@ -0,0 +1,15 @@
+package com.github.uguisu.www.algorithm.leetCode.g000401_000600.question509;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestCase {
+
+    @Test
+    public void test001() {
+        Solution s = new Solution();
+
+        assertEquals(5, s.fib(5));
+    }
+}
