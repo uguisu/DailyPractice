3ae083afe40900665722b9d9c7eed902213b87fa
kakin <ielts_go3@163.com>
2021-02-17 22:13:47 +0800

    leetcode 53

--- src/test/java/com/github/uguisu/www/algorithm/leetCode/g000001_000200/question053/TestCase.java
+++ src/test/java/com/github/uguisu/www/algorithm/leetCode/g000001_000200/question053/TestCase.java
@@ -14,4 +14,13 @@ public class TestCase {
 
         assertEquals(6, s.maxSubArray(nums));
     }
+
+    @Test
+    public void test003() {
+        int[] nums = new int[] {-2,1,-3,4,-1,2,1,-5,4};
+
+        Solution s = new Solution();
+
+        assertEquals(6, s.maxSubArray_003(nums));
+    }
 }
