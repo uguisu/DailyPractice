2134ebaa4dc2ce1a3088bf489dab9b96d7503600
uguisu <uguisu@users.noreply.github.com>
2024-03-03 00:46:56 +0800

    leetcode 27

--- src/main/java/com/github/uguisu/www/algorithm/leetCode/g000001_000200/question027/Solution.java
+++ src/main/java/com/github/uguisu/www/algorithm/leetCode/g000001_000200/question027/Solution.java
@@ -32,4 +32,62 @@ public class Solution {
         return i;
     }
 
+    // 20240303
+    public int removeElement2(int[] nums, int val) {
+        // verify
+        if(null == nums || 0 == nums.length) return 0;
+
+        // case: [1], target = 1
+        if(1 == nums.length) {
+            if(val == nums[0]) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+
+        // find the index of not target value, reverse order
+        int pLast = - 1;
+        for(int i = nums.length - 1; 0 <= i; i-- ) {
+            if(val != nums[i]) {
+                // find it
+                pLast = i;
+                break;
+            }
+        }
+
+        // case: [3, 3], target = 3
+        if(-1 == pLast) {
+            // pLast do not changed
+            return 0;
+        }
+
+        int tmp = 0;
+        int i = 0;
+        while(i < pLast) {
+            if(val == nums[i]) {
+                // find target value, change to last
+                tmp = nums[i];
+                nums[i] = nums[pLast];
+                nums[pLast] = tmp;
+
+                // move lasr point
+                pLast--;
+
+                while(i < pLast) {
+                    if(val != nums[pLast]) {
+                        // find it
+                        break;
+                    } else {
+                        pLast--;
+                    }
+                }
+            }
+
+            i++;
+        }
+
+        return pLast + 1;
+    }
+
 }
